78d97794a2eb099f9c4bda1b4fcf30bf549d6c61
2blon9 <2blon9@gmail.com>
2018-08-23 12:54:11 +0800

    Replaced most of the english text.

--- src/LangEn.js
+++ src/LangEn.js
@@ -144,62 +144,62 @@ export default {
     cicily: {
       title: 'Cicily',
       desc:
-        'DaoVentures 创始人，投资银行背景，曾主导或参与多个区块链项目的上市全流程，包括白皮书撰写、市场材料准备、投融资等；<br /><br />出生于上海，早年赴美留学与工作，坚定地希望通过区块链技术可以创建一个真正去中心化的良性生态；<br /><br />本科毕业于美国康乃尔大学地产金融系；<br /><br />负责 BLC 的商务拓展、运营策略、项目融资等。',
-      jobdesc: '负责 BLC 商务合作、运营策略、项目融资等'
+        'Co-founder of DaoVentures, investment banking background, ICO Advisor for several leading blockchain projects including ParcelX, DropChain, Robin8, InsChain, and Metaverse;<br />Born in Shanghai, attended university in the USA returned to China in 2014;<br />A strong believer in creating a decentralized ecosystem using blockchain technology;<br />Previous experience includes E&Y, PGC Capital, and Meixin Finance;<br />B. A in Business Administration from Cornel University, USA.',
+      jobdesc: 'In charge of business development, operations strategy and fundraising for BLC.'
     },
     terry: {
       title: 'Terry Tong',
       desc:
         '曾就职于 Groupon China 和 Oak Investment Partners，在互联网领域有丰富的产品研发和运营经验。擅长前端开发，并对区块链技术有深入的了解。热爱加密货币和区块链，坚信这将是改变人类社会的基石；<br /><br />本科毕业于香港科技大学计算机系并于美国麻省理工 Sloan 获得 MBA；<br /><br />负责 BLC 的国内社区运营以及产品的用户体验。',
-      jobdesc: '技术负责人，BLC 早期代码贡献者'
+      jobdesc: 'Main programmer, main contributor'
     },
     roald: {
       title: 'Roald Munoz',
       desc:
-        '国泰君安（中国前三大投行）的区块链研究中心负责人；数字货币项目的风险投资人，主要关注底层协议、数字身份、以及物联网硬件设施；<br /><br />出生于芝加哥，曾于美国空军情报分析处担任翻译，目前在上海生活，希望能帮助创建一个去中心化的经济系统；<br /><br />本科毕业于卡耐基梅隆的设计系，并于上海交通大学获得 MBA；<br /><br />负责 BLC 的海外社区运营以及海外商务拓展。',
-      jobdesc: '海外市场负责人'
+        'Senior Blockchain Industry Researcher at Guotai Junan Securities, a top 3 Chinese investment bank;<br />Crypto VC investor in decentralized technologies, with a focus on protocols, digitial identity, and IoT hardware infrastructure;<br />Raised in Chicago, previous experience in USAF intelligence analysis; now living and working in Shanghai to help create the future of a decentralized economy;',
+      jobdesc: 'In charge of overseas business development and overseas community management.'
     },
     calvin: {
       title: 'Calvin Poon',
       desc:
-        '曾于国泰君安担任区块链研究生和 OKEX 交易所协助上币业务；<br /><br />热爱区块链，深信区块链有如当九十年代互联网对世界的重大影响跟改变；<br /><br />本科毕业于美国波士顿大学经济和国际关系；<br /><br />负责协作 BLC 北美市场的社区管理（主要为加拿大）',
-      jobdesc: '加拿大社区运营经理'
+        'Researcher at Guotai Junan Securities, focusing on blockchain;<br />Business development analyst for OKEX, assisting with project listings;<br />Belief that blockchain will have a huge impact on the world similar to the advent of the internet;<br />B.A. from Boston University with a major in economics and international relations;',
+      jobdesc: 'In charge of community management for the North America market, with a focus on Canada.'
     },
     james: {
       title: 'James Kang',
       desc:
-        '曾实习于国泰君安研究所新能源领域；<br /><br />参与多项项目研究并拥有丰富的研究经验，对区块链技术有着浓厚的兴趣；<br /><br />本科毕业于波士顿大学经济系辅修商业管理；<br /><br />负责协作 BLC 北美市场的社区管理（主要为美国）',
-      jobdesc: '美国社区运营经理'
+        'Former intern at Guotai Junan Securities, taking part many projects via doing research and development;<br />Former leader of a high school rock band and a huge sports fan since the age of six;<br />B.A. from Boston University with a major in economics and minor in Business and Administration;',
+      jobdesc: 'In charge of community management for the North America market, with a focus on the USA.'
     },
     audrey: {
       title: 'Audrey Chen',
       desc:
-        '曾在硅谷知名的初创企业 Stroll Health 及 Thumbstack 担任产品经理和成长黑客；<br /><br />热衷于发展影响力投资和社会型企业，主要在食品及预防医疗领域；<br /><br />本科毕业于 Carleton 文理学院，曾创办 Carlhacks, 美国的一个黑客马拉松社区；<br /><br />负责协作 BLC 中国市场的社区管理。',
-      jobdesc: '中国社区运营经理'
+        'Product manager and growth hacker at Stroll Health & Thumbstack, a well-known startup in Silicon Valley;<br />Passionate about impact investment and social enterprises, focusing on food and health care industry;<br />B. A. from Carleton, founded Calhacks, a hackathon community in the U.S.;',
+      jobdesc: 'In charge of community management for China at BlC.'
     },
     czf: {
-      title: '成则丰',
+      title: 'Chen.zefeng',
       desc:
-        '从公司高管到互联网领域创业者，12年互联⽹从业经验<br /><br />2012年接触比特币，比特币早期矿工，做过套利，熟悉区块链原理，对区块链领域有着丰富的经验和兴趣。坚信区块链技术可以让人类生活更美好<br /><br />对计算机硬件技术发展有很深的了解，自学计算机课程，生活中自信开朗，喜欢军事类新闻节目<br /><br />负责BLC中国市场的社区管理、运营推⼴等相关⼯作',
-      jobdesc: '中国社区运营经理'
+        'An entrepreneur in internet startups with 12 years of internet work experience;<br />Learned about bitcoin in 2012, an early adopter for bitcoin mining, familiar with blockchain terminology with rich experience and interests in blockchain;<br />Deep understanding of computer hardware technology development and self-learner of computer programming;',
+      jobdesc: 'In charge of community management for China at BlC.'
     },
     billy: {
       title: 'Billy Chan',
       desc:
-        'Billy 是 IT 行业的资深人士，在数字技术软件开发、咨询和销售领域从业超过 15 年。他曾主导了微软在中国东部地区的软件反盗版工作；<br /><br />自 2015 年起，他开始对区块链颇具热情，并且对利用区块链来重塑食品和饮料分销行业倍感兴奋，并创立 DropChain，一家区块链供应链应用型初创企业；<br /><br />加拿大籍的华人，于 2014 年来到上海生活及工作。',
-      jobdesc: '技术顾问'
+        'Billy is a veteran of the IT industry, with over 15 years of software development, consulting, and sales experience in digital technology. Most recently he led the software antipiracy efforts for Microsoft in Eastern China;<br />He has been passionate about Blockchain since 2015 and is excited about leveraging it to reinvent the food and beverage distribution industry. He founded DropChain, a blockchain startup in supply chain;<br />Born in Canada, and went to Shanghai to live and work in 2014',
+      jobdesc: 'Technical advisor'
     },
     victor: {
       title: 'Victor Lee',
       desc:
-        'Victor 是市场营销行业的资深专家，担任奥姆尼康 (Omnicom) 传媒集团中国前副董事，专注于媒体广告、移动战略和品牌创新；<br /><br />对数字货币和区块链导向的初创公司充满热情，是一位经验丰富的连续创业者，并担任几个早期阶段区块链初创公司的导师；<br /><br />出生于马来西亚，于 2014 年来到上海生活及工作。',
-      jobdesc: '市场营销顾问'
+        'Victor is an expert in digital marketing, an ex- director in Omnicom Media Group China focused on media advertising, mobile strategies and brand innovation;<br />Passionate about digital disruption, blockchain & cryptocurrency startups. Experienced startup founder and mentor to several early stage startups;<br />Born in Malaysia, and went to Shanghai to live and work in 2014.',
+      jobdesc: 'Marketing advisor'
     },
     sioksiok: {
       title: 'Sioksiok Tan',
       desc:
         'Sioksiok 是一位资深记录片导演，曾担任 Discovery Asia 的监制，同时也是 Kinetic ONE 的创始人，一家拍摄年轻人、时尚和生活方式为主题的短视频平台，旗下拥有牛人库、优范儿、美丽极客三大独立频道；<br /><br />对区块链创业公司充满兴趣，并为 5 家以上的区块链创业公司成功制作视频并进行多渠道推广；<br /><br />出生于新加坡，于 2012 年移居北京生活及工作，经常往返于上海与北京两地。',
-      jobdesc: '战略顾问'
+      jobdesc: 'Strategic consultant'
     }
   },
   contactus: {
