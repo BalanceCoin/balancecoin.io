d140159874fcd5d364b951bbb47cd4145cb254d3
2blon9 <2blon9@gmail.com>
2018-09-02 02:51:19 +0800

    Fix image url from PoC history section.

--- src/LangCn.js
+++ src/LangCn.js
@@ -41,7 +41,7 @@ export default {
   },
   PoCHistory: {
     title: 'PoC 的历史',
-    picurl: 'image/poc-history.png',
+    picurl: 'images/poc-history.png',
     desc:
       '在比特币诞生两、三年后，人们逐渐承认了比特币的价值。而随着比特币价值逐步提高后，专有的 ASIC 矿机出现了，普通的非专业用户再也无法加入到挖矿的行列中来。此时区块链社区迫切的需要一种新的共识机制能够将 PoW 这种极度耗电的算法替代掉，让比特币重新回到大众的手中。'
   },
